e0ac7cbaf785b907fa8b97d688826cbef0aced53
sohrab <sohrab.hossain@bjitgroup.com>
2022-12-14 12:45:23 +0600

    update url

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,5 @@
-import { component$, Slot } from '@builder.io/qwik';
-import Header from '../components/header/header';
+import { component$, Slot } from "@builder.io/qwik";
+import Header from "../components/header/header";
 
 export default component$(() => {
   return (
@@ -11,8 +11,8 @@ export default component$(() => {
         </section>
       </main>
       <footer>
-        <a href="https://www.builder.io/" target="_blank">
-          Made with ♡ by Builder.io
+        <a href="https://qwik.builder.io/docs/getting-started/" target="_blank">
+          Qwik Documentation
         </a>
       </footer>
     </>
